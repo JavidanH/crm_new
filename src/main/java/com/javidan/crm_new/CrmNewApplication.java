2be792c450f6849ceea5ba4cbc92314638053140
JavidanH <jahajiyev@gmail.com>
2023-04-26 16:58:12 +0400

    first commit

---
+++ src/main/java/com/javidan/crm_new/CrmNewApplication.java
@@ -0,0 +1,13 @@
+package com.javidan.crm_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javidan/crm_new/CrmNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javidan.crm_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
